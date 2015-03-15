3b1f89db1ba9fd98990599d3ccb7df5a5d50139c
Allison McCartney <allison.n.mccartney@gmail.com>
2015-03-15 15:04:25 -0700

    Delete keys.py

--- keys.py
+++
@@ -1,6 +0,0 @@
-keys = dict(
-		consumer_key:         'lJLZPc334OMyEpktnLvRNxIgp',
-		consumer_secret:      'ewY1F8Mz8PltIzNf7Nt3yFwJJUEnV3rXM5m0Bt7Cn5QZ8PgYsF',
-		access_token:         '3077721756-cV9KVXlXfRzhRPefFxz9AmaaXiAeN4YUG28WmRr',
-		access_token_secret:  'BUloTjb2B7PMz3NJo2u0k16woQEelOpvTmcKaYK6eTi7z',
-	)
